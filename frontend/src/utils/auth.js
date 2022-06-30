1e8bf6729c8193dd9de29e54cbfcfe19f0160dbc
Andrey Bolshakov <abolshakovy@gmail.com>
2022-06-30 23:55:57 +0300

    Feature(baseURL) Update production baseUrl

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.bolshakov.nomoredomains.xyz';
 
 const checkResponse = (response) => {
     if (response.ok) {
